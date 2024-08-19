6e901fdb889b87b7f622a9de04ce8f6700da688e
Md Qaishar Zaki <109577871+md-qaishar-zaki@users.noreply.github.com>
2024-08-19 07:35:40 +0000

    .

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+ 
+const corsOptions = {
+  origin: 'http://localhost:5000/submit-form',  
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+};
 
-app.use(cors());
+app.use(cors(corsOptions)); 
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER, 
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
@@ -47,3 +53,8 @@ app.post('/submit-form', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the server!');
+});
+
